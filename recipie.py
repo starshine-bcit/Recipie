65289b436024addc6ffa0290f48568af4b42cb01
liliu <sfey1@my.bcit.ca>
2022-05-02 11:30:38 -0700

    Add argparse basics to recipie.py

--- recipie.py
+++ recipie.py
@@ -1,2 +1,30 @@
-# Skeleton main module file
 # Import from ./modules here
+from pathlib import Path
+import argparse
+
+def main():
+    #This section creates and parses arguments to the main program
+    parser = argparse.ArgumentParser(description='Recipie desktop application')
+
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action = 'store_true',
+        help = 'verbosely output to console'
+    )
+
+    #Change default to datastore locaion
+    parser.add_argument(
+        '-ds',
+        '--datastore',
+        type = Path,
+        default = Path('./data.json'),
+        help = 'json file to load, hopefully with recipies'
+    )
+
+    args = parser.parse_args()
+
+    #Call event loop here
+
+if __name__ == '__main__':
+    main()
