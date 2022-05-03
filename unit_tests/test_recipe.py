ae33a04968de19096dd6c9a90e13923d1bcf65cd
Sean <Waffuruki@hotmail.com>
2022-05-02 20:35:04 -0700

    Setting up unit-tests

---
+++ unit_tests/test_recipe.py
@@ -0,0 +1,67 @@
+import pytest
+
+recipe_json = """{
+    name: Apple Pie
+    ingredients: [
+        3 apples,
+        5 cups of flour,
+        2 cups of sugar,
+        10 sticks of butter
+    ]
+    description: "this is a recipe for apple pies"
+}"""
+
+def test_recipe():
+    pass
+
+def test_recipie():
+    pass
+
+def test_revent():
+    pass
+
+
+#test recipe class
+    """
+    test its attributes,
+    - name
+        string
+    - description
+        check if it's a string
+        more than 0
+    - ingredients
+        check if it's a list
+
+    - 
+
+    test the methods
+    - to_dictionary, serializes json to python dictionary
+    """
+    
+#test recipie main
+    """
+    test while loop
+    test methods to turn it into a string
+    errors?
+    - how to get out of the loop
+    - show input options?
+    - instructions
+    """
+
+#test recipe events
+    """
+    test the random recipe given
+    test the ingredients gives recipes with only those ingredients
+    test for recipes excluding ingredients
+    """
+
+#test json reader?
+    """
+    check if json
+    test open
+    test close
+    output is correct
+    """
+
+
+
