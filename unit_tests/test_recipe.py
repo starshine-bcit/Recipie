9ad87301ca48d88dc4794b3abcf5e489f02db7c5
Sean <Waffuruki@hotmail.com>
2022-05-02 23:19:36 -0700

    created test_recipe.py

--- unit_tests/test_recipe.py
+++ unit_tests/test_recipe.py
@@ -1,67 +1,58 @@
 import pytest
-
-recipe_json = """{
-    name: Apple Pie
-    ingredients: [
-        3 apples,
-        5 cups of flour,
-        2 cups of sugar,
-        10 sticks of butter
-    ]
-    description: "this is a recipe for apple pies"
-}"""
-
-def test_recipe():
-    pass
-
-def test_recipie():
-    pass
-
-def test_revent():
-    pass
+from ACIT2911.modules.recipe import Recipe  # For local testing
+# from modules.recipe import Recipe
 
 
-#test recipe class
+@pytest.fixture
+def rcp():
     """
-    test its attributes,
-    - name
-        string
-    - description
-        check if it's a string
-        more than 0
-    - ingredients
-        check if it's a list
+    Creates a Recipe instance, has a name, ingredients, and instructions
 
-    - 
-
-    test the methods
-    - to_dictionary, serializes json to python dictionary
+    Returns:
+        obj: Recipe instance
     """
-    
-#test recipie main
-    """
-    test while loop
-    test methods to turn it into a string
-    errors?
-    - how to get out of the loop
-    - show input options?
-    - instructions
-    """
-
-#test recipe events
-    """
-    test the random recipe given
-    test the ingredients gives recipes with only those ingredients
-    test for recipes excluding ingredients
-    """
-
-#test json reader?
-    """
-    check if json
-    test open
-    test close
-    output is correct
-    """
+    recipe = Recipe(
+        name="A Recipe", ingredients=[], instructions="These are cooking instructions"
+    )
+    return recipe
 
 
+def test_recipe(rcp):
+    """
+    Tests the Recipe class and ensures it has 3 attributes: name(str), ingredients(list), instructions(str)
 
+    Args:
+        rcp (obj): Recipe instance
+    """
+    assert rcp.name == "A Recipe"
+    assert isinstance(rcp.ingredients, list) == True
+    assert rcp.instructions == "These are cooking instructions"
+
+
+def test_recipe_errors():
+    """
+    Checks the Recipe class for errors, ensures that name is a string, ingredients is a list, and instructions is a string
+    """
+    name, ingr, instr = "Recipe Name", ["apple", "orange"], "instructions"
+
+    with pytest.raises(TypeError):
+        recipe = Recipe(12345, ingr, instr)
+    with pytest.raises(TypeError):
+        recipe = Recipe(name, "not a list", instr)
+    with pytest.raises(TypeError):
+        recipe = Recipe(name, ingr, ("not instructions", 1234))
+
+
+def test_recipe_methods(rcp):
+    """
+    Checks the Recipe class methods
+    to_dict - Ensure that it properly translates the class's attributes into dictionary form
+
+    Args:
+        rcp (obj): Recipe instance
+    """
+    assert rcp.to_dict() == {
+        "name": "A Recipe",
+        "ingredients": [],
+        "instructions": "These are cooking instructions",
+    }
