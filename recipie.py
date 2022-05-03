c65ad6c8e09127fb3fd5da04adc4d14911a24ea7
starshine <98191508+starshine-bcit@users.noreply.github.com>
2022-05-02 19:30:37 -0700

    Merge branch 'event' into branch 'dev' (#5)
    * Add argparse basics to recipie.py
    
    * Add imports to recipie.py, Add blank recent.py
    
    * Fix relative module imports, Add __init__.py files
    
    * Add cleaned recipe json to ./testing
    Add rstore.py json example
    
    * Add test Qt creator project to ./testing
    Add build_qtui.ps1 script
    Add compiled qtuitest.py
    Modify .gitignore for Qt debug files
    
    * Add ability to display main window in revent.py for testing
    Add show random recipe to revent.py for testing
    
    * Update .gitignore to inclue virtualenv files

--- recipie.py
+++ recipie.py
@@ -1,2 +1,38 @@
-# Skeleton main module file
+'''Recipie Main Module'''
 # Import from ./modules here
+from pathlib import Path
+import argparse
+import sys
+
+import modules.revent
+import modules.qtui
+
+def main():
+    #This section creates and parses arguments to the main program
+    parser = argparse.ArgumentParser(description='Recipie desktop application')
+
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action = 'store_true',
+        help = 'verbosely output to console'
+    )
+
+    #Change default to datastore locaion
+    parser.add_argument(
+        '-ds',
+        '--datastore',
+        type = Path,
+        default = Path('./data.json'),
+        help = 'json file to load, hopefully with recipies'
+    )
+
+    args = parser.parse_args()
+
+    # Call Qapplication and event loop here, example:
+    # app = QApplication(args)
+    # ex = Example()
+    # sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
