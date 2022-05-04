7c56a3e957f3a829b537fec601dc5eedb136f42c
liliu <sfey1@my.bcit.ca>
2022-05-04 12:19:19 -0700

    Adjust layout of Qt mainwindow Add image from logo branch Add image linking to Qt mainwindow

--- modules/revent.py
+++ modules/revent.py
@@ -1,6 +1,8 @@
 '''Recipie module for subclassing and event handling'''
 import sys
 from random import choice
+#import argparse
+#from pathlib import Path
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 
@@ -19,13 +21,27 @@ class MainWindowRecipie(Ui_MainWindow):
         self.actionRandom.triggered.connect(self.random_button_click)
         self.actionExit.triggered.connect(self.exit_recipie)
 
+    def linkimages(self, app):
+        '''Link image files to window'''
+        self.labelRecipieLogo.setPixmap(
+            QtGui.QPixmap('./image/RecipieLogo.png'))
+        app_icon = QtGui.QIcon()
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(16, 16))
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(24, 24))
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(32, 32))
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(48, 48))
+        app_icon.addFile('./image/RecipieLogo_icon.png',
+                         QtCore.QSize(256, 256))
+        app.setWindowIcon(app_icon)
+
     def random_button_click(self):
         '''Call rrecipe, then display it with display_recipe'''
         rrecipe = return_rrecipe(self.rlist)
-        if self.args.verbose: print(
-            f'Random event captured\n'
-            f'Selecting random recipe {rrecipe.name} and displaying...'
-        )
+        if self.args.verbose:
+            print(
+                f'Random event captured\n'
+                f'Selecting random recipe {rrecipe.name} and displaying...'
+            )
         self.display_recipe(
             rrecipe.name, rrecipe.ingredients, rrecipe.instructions)
 
@@ -37,7 +53,8 @@ class MainWindowRecipie(Ui_MainWindow):
 
     def exit_recipie(self):
         '''Exit the entire program'''
-        if self.args.verbose: print ('Exiting Recipie\n')
+        if self.args.verbose:
+            print('Exiting Recipie\n')
         sys.exit(0)
 
 
@@ -57,16 +74,43 @@ def return_rrecipe(rlist):
 
 
 def initmainwindow(args, rlist):
-    if args.verbose: print('Starting to initialize ui...')
-    app = QtWidgets.QApplication(args)
+    if args.verbose:
+        print('Starting to initialize ui...')
+    app = QtWidgets.QApplication([])
     MainWindow = QtWidgets.QMainWindow()
-    ui = MainWindowRecipie(args, rlist) #Instance of UI_MainWindow
-    ui.setupUi(MainWindow) #Basic initialization
-    ui.createevents() #Adding event listeners
+    ui = MainWindowRecipie(args, rlist)  # Instance of UI_MainWindow
+    ui.setupUi(MainWindow)  # Basic initialization
+    ui.createevents()  # Adding event listeners
+    ui.linkimages(app)  # Link images to window
     MainWindow.show()
-    if args.verbose: print('Initialized successfully and visible')
+    if args.verbose:
+        print('Initialized successfully and visible')
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
     pass
+    # parser = argparse.ArgumentParser(description='Recipie desktop application')
+
+    # parser.add_argument(
+    #     '-v',
+    #     '--verbose',
+    #     action = 'store_true',
+    #     help = 'verbosely output to console'
+    # )
+
+    # parser.add_argument(
+    #     '-ds',
+    #     '--datastore',
+    #     type = Path,
+    #     default = Path('./data'),
+    #     help = 'json file to load, hopefully with recipies'
+    # )
+
+    # args = parser.parse_args()
+    # if args.verbose: print(
+    #     f'Parsed command line arguments:'
+    #     f'\n{args}\n'
+    # )
+    # rlist = []
+    # initmainwindow(args, rlist)
