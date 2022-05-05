ee6a60beb1733af63670ad440cfdcb7e086f869b
starshine <98191508+starshine-bcit@users.noreply.github.com>
2022-05-04 20:53:50 -0700

    Merge branch 'event' into 'dev' for deliverable 1 (#7)
    * Update README.md (#1)
    
    Updated the acknowledgements
    
    * Add argparse basics to recipie.py
    
    * Add imports to recipie.py, Add blank recent.py
    
    * Fix relative module imports, Add __init__.py files
    
    * Add cleaned recipe json to ./testing
    Add rstore.py json example
    
    * Add test Qt creator project to ./testing
    Add build_qtui.ps1 script
    Add compiled qtuitest.py
    Modify .gitignore for Qt debug files
    
    * Add ability to display main window in revent.py for testing
    Add show random recipe to revent.py for testing
    
    * Update .gitignore to inclue virtualenv files
    
    * Fix layout of qttest project to scale with drag
    
    * Fix scaling of ui with minimum size
    
    * Clean up code in preparation of integration
    Remove references and use of testing in ./modules and recipie.py
    
    * Adjust layout of Qt mainwindow
    Add image from logo branch
    Add image linking to Qt mainwindow
    
    * Update recipie.py to pass verbose instead of args
    Update recipie and revent modules to interface with io modules
    
    * Fix code readability in a few sections
    
    * Fix minor bugs in preparation of merge to dev
    
    * Update revent.py to reflect new methods in io/recipe_class branchs
    
    * Add run_recipie.ps1 shell script
    
    * Update .gitignore for graphviz utility
    
    Co-authored-by: SeanXYTan <90466609+SeanXYTan@users.noreply.github.com>

--- modules/revent.py
+++ modules/revent.py
@@ -1,54 +1,118 @@
 '''Recipie module for subclassing and event handling'''
 import sys
+from random import choice
+#import argparse
+#from pathlib import Path
+
 from PyQt6 import QtCore, QtGui, QtWidgets
 
-# To make an environment to reach other folder
-# $ENV:PYTHONPATH = "C:\Courses\ACIT-2911\project\ACIT2911"
-
-from testing.qtuitest import Ui_MainWindow
-from testing.rstore import return_rrecipe
-# Subclass things here for events from mainwindow?
+from .qtui import Ui_MainWindow
 
 
-class MainWindowTest(Ui_MainWindow):
-    def __init__(self) -> None:
+class MainWindowRecipie(Ui_MainWindow):
+    def __init__(self, verbose, rlist) -> None:
         super().__init__()
+        self.rlist = rlist
+        self.verbose = verbose
 
-    def updateui(self):
+    def createevents(self):
+        '''Connect triggered events with functions'''
         self.pushButtonRandom.clicked.connect(self.random_button_click)
+        self.actionRandom.triggered.connect(self.random_button_click)
+        self.actionExit.triggered.connect(self.exit_recipie)
+
+    def linkimages(self, app):
+        '''Link image files to window'''
+        self.labelRecipieLogo.setPixmap(
+            QtGui.QPixmap('./image/RecipieLogo.png'))
+        app_icon = QtGui.QIcon()
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(16, 16))
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(24, 24))
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(32, 32))
+        app_icon.addFile('./image/RecipieLogo_icon.png', QtCore.QSize(48, 48))
+        app_icon.addFile('./image/RecipieLogo_icon.png',
+                         QtCore.QSize(256, 256))
+        app.setWindowIcon(app_icon)
 
     def random_button_click(self):
-        title, ingred, instruct = parserecipe(return_rrecipe())
-        # print(title)
-        # print(ingred)
-        # print(instruct)
-        self.display_recipe(title, ingred, instruct)
+        '''Call rrecipe, then display it with display_recipe'''
+        rrecipe = self.rlist.get_random_recipe()
+        if self.verbose:
+            print(
+                f'Random event captured\n'
+                f'Selecting random recipe {rrecipe.name} and displaying...'
+            )
+        ingreds = rrecipe.ingredients_as_str()
+        self.display_recipe(
+            rrecipe.name, ingreds, rrecipe.instructions)
 
     def display_recipe(self, title, ingred, instruct):
+        '''Display chosen recipe in the main window'''
         self.labelRecipeName.setText(title)
         self.textBrowserRecipeIngredients.setText(ingred)
         self.textBrowserRecipeDirections.setText(instruct)
 
-
-def parserecipe(rrecipe):
-    title = rrecipe.get('title')
-    ingred = rrecipe.get('ingredients')
-    ingred_parsed = ''
-    for x in ingred:
-        ingred_parsed += x + '\n'
-    instruct = rrecipe.get('instructions')
-    return title, ingred_parsed, instruct
+    def exit_recipie(self):
+        '''Exit the entire program'''
+        if self.verbose:
+            print('Exiting Recipie\n')
+        sys.exit(0)
 
 
-def main():
+# def return_rrecipe(rlist):
+#     '''Return a random recipe class instance from a list of them'''
+#     return choice(rlist.recipes)
+
+
+# def parserecipe(rrecipe):
+#     title = rrecipe.get('title')
+#     ingred = rrecipe.get('ingredients')
+#     ingred_parsed = ''
+#     for x in ingred:
+#         ingred_parsed += x + '\n'
+#     instruct = rrecipe.get('instructions')
+#     return title, ingred_parsed, instruct
+
+
+def initmainwindow(verbose, rlist):
+    '''Initialize and display main recipie window'''
+    if verbose:
+        print('Starting to initialize ui...')
     app = QtWidgets.QApplication([])
     MainWindow = QtWidgets.QMainWindow()
-    ui = MainWindowTest()
-    ui.setupUi(MainWindow)
-    ui.updateui()
+    ui = MainWindowRecipie(verbose, rlist)  # Instance of UI_MainWindow
+    ui.setupUi(MainWindow)  # Basic initialization
+    ui.createevents()  # Adding event listeners
+    ui.linkimages(app)  # Link images to window
     MainWindow.show()
+    if verbose:
+        print('Initialized successfully and visible')
     sys.exit(app.exec())
 
 
 if __name__ == '__main__':
-    main()
+    pass
+    # parser = argparse.ArgumentParser(description='Recipie desktop application')
+
+    # parser.add_argument(
+    #     '-v',
+    #     '--verbose',
+    #     action = 'store_true',
+    #     help = 'verbosely output to console'
+    # )
+
+    # parser.add_argument(
+    #     '-ds',
+    #     '--datastore',
+    #     type = Path,
+    #     default = Path('./data'),
+    #     help = 'json file to load, hopefully with recipies'
+    # )
+
+    # args = parser.parse_args()
+    # if verbose: print(
+    #     f'Parsed command line arguments:'
+    #     f'\n{args}\n'
+    # )
+    # rlist = []
+    # initmainwindow(args, rlist)
--- recipie.py
+++ recipie.py
@@ -1,38 +1,55 @@
 '''Recipie Main Module'''
-# Import from ./modules here
 from pathlib import Path
 import argparse
-import sys
+
+#from PyQt6 import QtCore, QtGui, QtWidgets
 
 import modules.revent
-import modules.qtui
+#import modules.qtui
+import modules.recipelist
+
+
+def createdslist(args):
+    '''Create a list of files from the supplied directory'''
+    datafilelist = list(args.datastore.glob('*.json'))
+    if len(datafilelist) < 1:
+        raise ValueError('Error: no JSON datastores found')
+    return datafilelist
+
 
 def main():
-    #This section creates and parses arguments to the main program
+    '''Main recipie function to initiate/load everything else'''
     parser = argparse.ArgumentParser(description='Recipie desktop application')
 
     parser.add_argument(
         '-v',
         '--verbose',
-        action = 'store_true',
-        help = 'verbosely output to console'
+        action='store_true',
+        help='verbosely output to console'
     )
 
-    #Change default to datastore locaion
     parser.add_argument(
         '-ds',
         '--datastore',
-        type = Path,
-        default = Path('./data.json'),
-        help = 'json file to load, hopefully with recipies'
+        type=Path,
+        default=Path('./data'),
+        help='json file to load, hopefully with recipies'
     )
 
     args = parser.parse_args()
+    if args.verbose:
+        print(
+            f'Parsed command line arguments:'
+            f'\n{args}\n'
+        )
 
-    # Call Qapplication and event loop here, example:
-    # app = QApplication(args)
-    # ex = Example()
-    # sys.exit(app.exec())
+    verbose = True if args.verbose else False
+    datafilelist = createdslist(args)
+    mainrlist = modules.recipelist.RecipeList(datafilelist)
+    if verbose:
+        print(f'Total recipes indexed: {len(mainrlist.recipes)}')
+    modules.revent.initmainwindow(verbose, mainrlist)
+
 
 if __name__ == '__main__':
     main()
