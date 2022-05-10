1a939ac093d6bdff6a3309be62b250eea2b16ee5
Sean <Waffuruki@hotmail.com>
2022-05-10 13:13:32 -0700

    Fixed one of the asserts and seperated the tests

--- unit_tests/test_exact_search.py
+++ unit_tests/test_exact_search.py
@@ -1,3 +1,4 @@
+import pytest
 # from modules.esearch import ExactSearch
 from ACIT2911.modules import esearch
 from ACIT2911.modules.recipe import Recipe
@@ -36,13 +37,34 @@ RECIPES_LIST = [
 ]
 
 
-def test_exact_search():
+@pytest.fixture
+def search():
+    result = esearch(USER_INPUT, RECIPES_LIST)
+    return result 
+
+
+def test_exact_search(search):
     """
     Output should be a list
     Each item in the list should be a Recipe instance
     Capitalization shouldn't matter for the user input
     """
-    result = esearch(USER_INPUT)
-    assert isinstance(result, list) == True
-    assert isinstance(result[0], Recipe) == True
-    assert result.title == "Apple Pie"
+    #The first argument is the user's input, this user input is a list of ingredients, the second argument is the list of Recipe instances
+    result = esearch(USER_INPUT, RECIPES_LIST)
+
+    #To access the ingredients key inside the RECIPES_LIST, iterate over it and use .ingredients with the iterated Recipe instance eg.for recipe in RECIPE_LIST, if recipe.ingredients...       
+
+
+def test_exact_search_output(search):
+    #A list should be returned from the function
+    assert isinstance(search, list) == True
+
+
+def test_exact_search_output_item(search):
+    #The list should contain only Recipe instances/class
+    assert isinstance(search[0], Recipe) == True
+
+
+def test_exact_search_working(search):
+    #Checks if the exact match function worked, since only one item in the list matches those same ingredients
+    assert search[0].title == "Apple Pie"  
