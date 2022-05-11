7510d2c83bee730f19f1f4684983396a52ada2bd
liliu <sfey1@my.bcit.ca>
2022-05-11 16:28:36 -0700

    Maybe fix search functionality

--- modules/esearch.py
+++ modules/esearch.py
@@ -1,5 +1,9 @@
+from .recipe import Recipe
+from .recipelist import RecipeList
+import string
 
-def exact_search(ingredient_input: list[str], recipes_list: list[dict]) -> list[dict]:
+
+def exact_search(ingredient_input: list[str], recipes_list: RecipeList) -> list[Recipe]:
     matched_recipe = [] 
     user_input = [] 
 
@@ -20,36 +24,41 @@ def exact_search(ingredient_input: list[str], recipes_list: list[dict]) -> list[
         else: 
             user_input.append(user_ingred)
 
-    for recipe in recipes_list: 
+    for recipe in recipes_list.recipes: 
         match = True 
-        ingred_list = [] 
         new_ingred_list = [] 
 
-        for ingredients in recipe["ingredients"]:
-            ingred = ingredients.split(" ") # List of items  
-            ingred_list.append(ingred[-1]) # Append last word of a string 
+        ingred_list = [i.lower().translate(str.maketrans(
+            '', '', string.punctuation)) for i in recipe.ingredients]
             
-        for food in ingred_list:
-            if food.endswith('ies'): 
-                food_item = food.replace('ies', 'y')
-                new_ingred_list.append(food_item)
-                # new_ingred_list.append(food.replace('ies', 'y'))
-            elif food.endswith('s'):
-                food_item = food.replace('s', '')
-                new_ingred_list.append(food_item)
-                # new_ingred_list.append(food.replace('s', ''))                     
-            elif food.endswith('es'):
-                food_item = food.replace('es', '')
-                new_ingred_list.append(food_item)
-                # new_ingred_list.append(food.replace('es', ''))
-            else: 
-                new_ingred_list.append(food)
+        templist = []
+        for ingredstring in ingred_list:
+            for food in ingredstring.split(' '):
+                if food.endswith('ies'): 
+                    food_item = food.replace('ies', 'y')
+                    # new_ingred_list.append(food.replace('ies', 'y'))
+                elif food.endswith('s'):
+                    food_item = food.replace('s', '')
+                    # new_ingred_list.append(food.replace('s', ''))                     
+                elif food.endswith('es'):
+                    food_item = food.replace('es', '')
+                    # new_ingred_list.append(food.replace('es', ''))
+                else: 
+                    food_item = food
+                templist.append(food_item)
+                
+            new_ingred_list.append(templist)
+            templist = []
 
-        for new_food in new_ingred_list: 
-            if new_food not in user_input:
+        for ingred in new_ingred_list:
+            ret = -1
+            for term in user_input:
+                fret = ingred.find(term)
+                if fret > ret:
+                    ret = fret
+            if ret == -1:
                 match = False
-
-        if match is True: 
+        if match is True:
             matched_recipe.append(recipe)
 
-    return matched_recipe 
+    return matched_recipe
