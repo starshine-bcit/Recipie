e82628fbb4073218d205fb39a128df422ca36945
Amanda Chang <achang140@my.bcit.ca>
2022-05-11 16:16:01 -0700

    Added a new function (replace_chr) to esearch.py to make exact_search function shorter

--- modules/esearch.py
+++ modules/esearch.py
@@ -1,49 +1,32 @@
+def replace_chr(words_list:list[str]) -> list[str]:
+    word_list = []
+    for words in words_list: 
+        word = words.lower()
+        if word.endswith('ies'):
+            word_list.append(word.replace('ies', 'y'))
+        elif word.endswith('s'):
+            word_list.append(word.replace('s', ''))
+        elif word.endswith('es'):
+            word_list.append(word.replace('es', ''))
+        else: 
+            word_list.append(word)
+    
+    return word_list 
+        
 
 def exact_search(ingredient_input: list[str], recipes_list: list[dict]) -> list[dict]:
     matched_recipe = [] 
-    user_input = [] 
-
-    for user in ingredient_input: 
-        user_ingred = user.lower()
-        if user_ingred.endswith('ies'): 
-            ingred_input = user_ingred.replace('ies', 'y')
-            user_input.append(ingred_input)
-            # user_input.append(user_ingred.replace('ies', 'y'))
-        elif user.endswith('s'):
-            ingred_input = user_ingred.replace('s', '')
-            user_input.append(ingred_input)
-            # user_input.append(user_ingred.replace('s', ''))
-        elif user.endswith('es'):
-            ingred_input = user_ingred.replace('es', '')
-            user_input.append(ingred_input)
-            # user_input.append(user_ingred.replace('es', ''))
-        else: 
-            user_input.append(user_ingred)
+    user_input = replace_chr(ingredient_input) # List of ingredients user input 
 
     for recipe in recipes_list: 
         match = True 
         ingred_list = [] 
-        new_ingred_list = [] 
 
         for ingredients in recipe["ingredients"]:
             ingred = ingredients.split(" ") # List of items  
             ingred_list.append(ingred[-1]) # Append last word of a string 
-            
-        for food in ingred_list:
-            if food.endswith('ies'): 
-                food_item = food.replace('ies', 'y')
-                new_ingred_list.append(food_item)
-                # new_ingred_list.append(food.replace('ies', 'y'))
-            elif food.endswith('s'):
-                food_item = food.replace('s', '')
-                new_ingred_list.append(food_item)
-                # new_ingred_list.append(food.replace('s', ''))                     
-            elif food.endswith('es'):
-                food_item = food.replace('es', '')
-                new_ingred_list.append(food_item)
-                # new_ingred_list.append(food.replace('es', ''))
-            else: 
-                new_ingred_list.append(food)
+        
+        new_ingred_list = replace_chr(ingred_list)
 
         for new_food in new_ingred_list: 
             if new_food not in user_input:
