4f4d29ea67da0308de110835b306c776c258bdf8
Raqin Shikder <raqin2003@gmail.com>
2022-05-11 21:55:18 -0700

    fixed psearch code as last iteration of psearch technically passed the pytests but did not work as intended

--- modules/psearch.py
+++ modules/psearch.py
@@ -3,17 +3,14 @@
 """A function that takes in a list of ingredients and a list of recipes
 and returns matching recipes (that use all ingredients) as a list"""
 
+
+
 def p_search(ingredients_list: list[str], recipes_list: list[dict]) -> list[dict]:
         matched = []
         for recipe in recipes_list:
-                for words in recipe['ingredients']:
-                        food = words.split()
-                for ingred in ingredients_list:
-                        if ingred in food:
-                            matched.append(recipe)
-                                    
-
-
-                    
-        return(matched)    
+            for sentence in recipe['ingredients']:
+                all_word = sentence.split()
+            if (any(n in ingredients_list for n in all_word)) == True:
+                matched.append(recipe)    
+        return(matched)   
     
