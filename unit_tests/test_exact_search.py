eb609bdda4f26d858c5ddd78322de4dd78b17770
SeanXYTan <90466609+SeanXYTan@users.noreply.github.com>
2022-05-12 20:52:52 -0700

    Merge esearch_module into dev for deliverable 2 (#17)
    * Create unit test for exact match function
    
    * Fixed one of the asserts and seperated the tests
    
    * Made test_exact_search.py easier
    
    * Fixed an assert that checks the list for Recipe instances, when none were made
    
    * Adjusted docstrings to the appropriate documentation
    
    * Refactored test with an additional check for user input capitalization
    
    * Modified test_exact_search.py (changed import statement and a function name) & Added exact_search function to esearch.py
    
    * Added additional tests and 2 additional things in the RECIPE_LIST to test the new function
    
    * Added additional code for exact_function to handle singular and plural words comparison
    
    * Added a new function (replace_chr) to esearch.py to make exact_search function shorter
    
    * Refactored code to pass the last test
    
    * Refactored code and added documentation
    
    * Add test for punctuation in the ingredients list
    
    * Refactored for integration with RecipeList
    
    * esearch V1.1, should search exactly this time
    
    * esearch v1.2, more fixing
    
    * esearch v1.3, Changed the way exclusion effect works
    
    * Removed debugging code
    
    * Updated to stop after finding the ingredient
    
    * esearch v1.5, last refactor before the deliverable
    
    * secret push to fix recipelist unit test, since the file it relies on was updated
    
    Co-authored-by: Amanda Chang <achang140@my.bcit.ca>

--- unit_tests/test_exact_search.py
+++ unit_tests/test_exact_search.py
@@ -1,67 +1,28 @@
 import pytest
-from ACIT2911.modules.esearch import exact_search
+from pathlib import Path
+from modules.recipe import Recipe
+from modules.recipelist import RecipeList
+from modules.esearch import exact_search
+
 
 USER_INPUT = ['flour', 'sugar', 'butter', 'apples']
 ANNOYING_USER_INPUT = ['Flour', 'SUGAR', 'butTeR', 'blueBERRIES']
 SINGULAR_USER_INPUT = ['Flour', 'sugar', 'Butter', 'apple']
-ONION_ENJOYER_INPUT = ['bread', 'onion', 'ham', 'onion']
-
-RECIPES_LIST = [
-    {
-        "title": "Apple Pie",
-        "ingredients": [
-            "3 apples",
-            "5 cups of flour",
-            "2 cups of sugar",
-            "10 sticks of butter"
-        ],
-        "instructions": "this is a recipe for apple pies"
-    },
-    {
-        "title": "Blueberry Pie",
-        "ingredients": [
-            "2 cups of blueberries",
-            "5 cups of flour",
-            "2 cups of sugar",
-            "10 sticks of butter"
-        ],
-        "instructions": "this is a recipe for blueberry pies"
-    },
-    {
-        "title": "Death",
-        "ingredients": [
-            "3 cups of coca cola",
-            "2 sticks of mentos"
-        ],
-        "instructions": "drink the cola then down the mentos"
-    },
-    {
-        "title": "Sandwich with Onions",
-        "ingredients": [
-            "2 slices of bread",
-            "1 whole onion",
-            "2 slices of ham",
-            "1 lettuce leaf"
-        ],
-        "instructions": "Assemble this very sad sandwich that is meant to test the onion thingo"
-    },    
-    {
-        "title": "Onion Soup & Bread",
-        "ingredients": [
-            "1 can of onion soup",
-            "3 slices of bread",
-            "1 leg of ham",
-            "1 whole head of lettuce"
-        ],
-        "instructions": "Toast the bread and drink the soup, eat like a barbarian."
-    },
-
-]
+ONION_ENJOYER_INPUT = ['bread', 'onion', 'ham', 'lettuce']
+ONION_SOUP_ENJOYER_INPUT = ['bread', 'onion soup', 'ham', 'lettuce']
+WEIRD_USER_INPUT = ['strawberry', 'chicken', 'broccoli']
 
 
 @pytest.fixture
-def search():
-    result = exact_search(USER_INPUT, RECIPES_LIST)
+def rlist():
+    path = Path("./unit_tests/example.json")
+    recipelist = RecipeList([path])
+    return recipelist
+
+
+@pytest.fixture
+def search(rlist):
+    result = exact_search(USER_INPUT, rlist)
     return result 
 
 
@@ -79,32 +40,48 @@ def test_exact_search(search):
 
 def test_exact_search_output_item(search):
     #The list should contain only dictionaries
-    assert isinstance(search[0], dict) == True
+    assert isinstance(search[0], Recipe) == True
+
 
 
 def test_exact_search_working(search):
     #Checks if the exact match function worked, since only one item in the list matches those same ingredients
-    assert search[0]['title'] == "Apple Pie"  
+    assert search[0].name == "Apple Pie"  
 
 
-def test_irregular_input():
+def test_irregular_input(rlist):
     #Checks if the exact match function works even if the user doesn't know how to turn off their caps or stop holding shift
-    result = exact_search(ANNOYING_USER_INPUT, RECIPES_LIST)
+    result = exact_search(ANNOYING_USER_INPUT, rlist)
     assert isinstance(result, list) == True
-    assert result[0]['title'] == "Blueberry Pie"
+    assert result[0].name == "Blueberry Pie"
 
 
-def test_plural_input():
+def test_plural_input(rlist):
     #Checks if the exact_search function works even if the user forgets to make the word plural
-    
-    result = exact_search(SINGULAR_USER_INPUT, RECIPES_LIST)
+    result = exact_search(SINGULAR_USER_INPUT, rlist)
     assert isinstance(result, list) == True
-    assert result[0]['title'] == "Apple Pie"
+    assert result[0].name == "Apple Pie"
 
 
-def test_exact_ingredient():
+def test_exact_ingredient(rlist):
     #Checks if it only matches with the ingredient and not ingredients with that share the same ingredient, eg. onions and onion soup
-    result = exact_search(ONION_ENJOYER_INPUT, RECIPES_LIST)
+    result = exact_search(ONION_ENJOYER_INPUT, rlist)
     assert isinstance(result, list) == True
     assert len(result) == 1
-    assert result[0]['title'] == "Sandwich with Onions"
+    assert result[0].name == "Sandwich with Onions"
+
+
+def test_exact_ingredient_two(rlist):
+    #Checks if it only matches with the ingredient and not ingredients with that share the same ingredient, eg. onions and onion soup
+    result = exact_search(ONION_SOUP_ENJOYER_INPUT, rlist)
+    assert isinstance(result, list) == True
+    assert len(result) == 1
+    assert result[0].name == "Onion Soup & Bread"
+
+
+def test_for_punctuated_ingredients(rlist):
+    #Checks for weirdly punctuated recipes, if any
+    result = exact_search(WEIRD_USER_INPUT, rlist)
+    assert isinstance(result, list) == True
+    assert len(result) == 1
+    assert result[0].name == "Weirdly Punctuated Recipe"
