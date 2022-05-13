fe84cd04b9184ab1d316cf479e85913c6d25c837
Eva Chu <echu58@my.bcit.ca>
2022-05-12 19:31:22 -0700

    Update order of looping, use regex

--- modules/psearch.py
+++ modules/psearch.py
@@ -1,12 +1,13 @@
 """A function that takes in a list of ingredients and a list of recipes
 and returns matching recipes (that use all ingredients) as a list"""
 
-from modules.recipe import Recipe
-from modules.recipelist import RecipeList
+import re
+# from modules.recipe import Recipe
+# from modules.recipelist import RecipeList
 
 # When testing, comment out above import blocks & uncomment below imports
-# from ACIT2911.modules.recipe import Recipe
-# from ACIT2911.modules.recipelist import RecipeList
+from ACIT2911.modules.recipe import Recipe
+from ACIT2911.modules.recipelist import RecipeList
 
 
 def p_search(ingredients_list: list[str],
@@ -39,18 +40,24 @@ def p_search(ingredients_list: list[str],
         # Start counting for ingredient matches in recipe
         matches = 0
 
-        # Iterate through each ingredient in recipe
-        for rec_ingredient in recipe.ingredients:
-            # Iterate through each input ingredient
-            for ingredient in ingredients_list:
+        # Iterate through each input ingredient
+        for ingredient in ingredients_list:
+            # Iterate through each ingredient in recipe
+            for rec_ingredient in recipe.ingredients:
 
                 # Stop looping if number of matches is met
                 if matches == required_matches:
                     break
-                # Stop looping if input ingredient is
-                #  in current recipe ingredient and increment
-                #  if ingredients match
-                if ingredient in rec_ingredient:
+                # Set up regex to look for possibly plural ingredient
+                #  and commas and spaces to single out ingredient
+                to_match = f'^{ingredient}[ies]*[,\\s]+|'\
+                           f'[,\\s]+{ingredient}[ies]*$|'\
+                           f'[,\\s]+{ingredient}[ies]*[,\\s]+|'\
+                           f'^{ingredient}$'
+                match = re.search(to_match, rec_ingredient)
+                # Stop looping if input ingredient is in current
+                #  recipe ingredient, increment if ingredients match
+                if match:
                     matches += 1
                     break
 
