f2d7cbb6e9641ff44bbcc4a75261ab110a8d7379
Eva Chu <echu58@my.bcit.ca>
2022-05-17 16:41:33 -0700

    Update tests and code to match desired format

--- modules/recipelist.py
+++ modules/recipelist.py
@@ -1,5 +1,6 @@
 """Module of a class of a list of food recipes."""
 
+from dis import Instruction
 import json
 import random
 from pathlib import Path
@@ -22,9 +23,6 @@ class RecipeList:
     def __init__(self, files: list[Path]):
         self.recipes = []
 
-        # Numbering recipes for easy reference.
-        count = 1
-
         for file in files:
             with file.open('r', encoding='utf-8') as fp:
                 data = json.load(fp)
@@ -36,10 +34,10 @@ class RecipeList:
                             id = int(key),
                             name = value["title"],
                             ingredients = value["ingreds"],
-                            instructions = value["instruct"]
+                            instructions = value["instruct"],
+                            diets = ['placeholder']
                         )
                     )
-                    count += 1
                 except:
                     # Quick check of where it is breaking
                     # print(self.recipes[-1].name)
@@ -54,5 +52,45 @@ class RecipeList:
     # Method used when cleaning up multiple recipe data files.
     def save_recipes_to_file(self):
         """Writes recipes to JSON file"""
-        with open("./data/recipe_list.json", "w") as fp:
-            json.dump([rec.to_dict() for rec in self.recipes], fp)
+        recipes_json = {}
+        count = 0
+        with open("./data/recipe_list_test_test.json", "w") as fp:
+            for recipe in self.recipes:
+                recipes_json.update(
+                    {count: {
+                        "title": recipe.name,
+                        "ingreds": recipe.ingredients,
+                        "instruct": recipe.instructions,
+                        "cat": []
+                    }}
+                )
+                count +=1
+            json.dump(recipes_json, fp, indent = 4)
+
+
+    # WORK IN PROGRESS
+    # Get list of recipes that belong to diet
+    # def add_diet_recipes(
+    #         self,
+    #         label: str,
+    #         exclude_ingreds: list[str]
+    # ) -> list[Recipe]:
+
+    #     results_list = []
+    #     for recipe in self.recipes:
+    #         rec_ingreds = recipe.ingredients_as_str()
+    #         matches = 0
+    #         for ingred in exclude_ingreds:
+    #             if ingred in rec_ingreds:
+    #                 break
+    #             if ingred == exclude_ingreds[-1]:
+    #                 recipe.diets.append(f'{label}')
+    #                 results_list.append(recipe)
+    #     return results_list
+
+    # # Pass above return into below fnx
+
+    # # Add label to diet recipes
+    # def add_diet_label(self, label, diet_recipes: list[Recipe]):
+    #     for d_rec in diet_recipes:
+    #         pass
