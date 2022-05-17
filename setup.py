165f8d28f13cfb0da01252cfed01a40ba4c39b23
liliu <sfey1@my.bcit.ca>
2022-05-17 14:25:17 -0700

    Change version number for test package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import pathlib
 from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
-VERSION = '0.2.2'
+VERSION = '0.2.2.1'
 PACKAGE_NAME = 'recipie'
 AUTHOR = 'starshine-bcit'
 AUTHOR_EMAIL = 'sfey1@my.bcit.ca'
