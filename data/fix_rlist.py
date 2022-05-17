db5ea90a317dacc5f01a094ec83910f16cc4190b
liliu <sfey1@my.bcit.ca>
2022-05-17 11:28:20 -0700

    Remove old json from dev merge, Remove orphaned code from fix_rlist.py

--- data/fix_rlist.py
+++ data/fix_rlist.py
@@ -37,31 +37,4 @@ if len(rdict) > 0:
     outfile = Path('cleaned_recipes.json')
     with outfile.open('w', encoding='utf-8') as fobject:
         json.dump(rdict, fobject, indent = 4)
-
-# for file in jsonfiles:
-#     with file.open('r', encoding='utf-8') as fobject:
-#         dfile = json.load(fobject)
-
-#     for val in dfile.values():
-#         try:
-#             if len(val['title']) < 1 or len(val['ingredients']) < 1 or len(val['instructions']) < 1:
-#                 continue
-#             title = val['title']
-#             title.translate(str.maketrans('', '', string.punctuation))
-#             ingreds = [i for i in val['ingredients'] if i != '\n']
-#             rdict.update({
-#                 count: {
-#                     'title': title,
-#                     'ingred': ingreds,
-#                     'instruct': val['instruct'],
-#                     'cat': 'Null'
-#                 }
-#             })
-#             count += 1
-#         except:
-#             continue
-#     print(len(rdict))
-# if len(rdict) > 100000:
-#     outfile = Path('cleaned_recipes')
-#     with outfile.open('r', encoding='utf-8') as fobject:
-#         json.dump(rdict, outfile, indent = 4)
+        
