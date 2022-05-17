0df70a7190cf1c47cc1eb0d4d3dd8df42f43edb3
Eva Chu <echu58@my.bcit.ca>
2022-05-16 21:17:56 -0700

    Update recipe module with id and diet attributes

--- modules/recipe.py
+++ modules/recipe.py
@@ -4,31 +4,67 @@ class Recipe:
     """A class that represents a food recipe.
 
     Attributes:
+        id (int):
+            ID number of recipe when in a list.
         name (str):
             Name of the recipe.
+        diet (list[str]):
+            Dietary labels/categories of the recipe.
         ingredients (list[str]):
             Ingredients used in the recipe.
         instructions (str):
             Instructions for how to make the recipe.
     """
 
-    def __init__(self, name: str, ingredients: list[str], instructions: str):
+    def __init__(
+            self,
+            id: int,
+            name: str,
+            diets: list[str],
+            ingredients: list[str],
+            instructions: str
+        ):
+
+        if type(id) != int:
+            raise TypeError(
+                "Recipe ID improperly formatted, should be an integer."
+            )
+
         if type(name) != str:
             raise TypeError(
                 "Recipe name improperly formatted, should be string."
             )
+
+        if type(diets) != list:
+            raise TypeError(
+                "Recipe diet labels improperly formatted, should be a list."
+            )
         
+        for label in diets:
+            if type(label) != str:
+                raise TypeError(
+                    "Recipe diet label should be string."
+                )
+
         if type(ingredients) != list:
             raise TypeError(
                 "Recipe ingredients improperly formatted, should be a list."
             )
+
+        for ingred in ingredients:
+            if type(ingred) != str:
+                raise TypeError(
+                    "Each ingredient in recipe should be string."
+                )
 
         if type(instructions) != str:
             raise TypeError(
                 "Recipe instructions improperly formatted, should be string."
             )
 
+        self.id = id
         self.name = name
+        self.diets = diets
         self.ingredients = ingredients
         self.instructions = instructions
 
