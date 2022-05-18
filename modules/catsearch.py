64f32fa84ee8470d675ee6d4ae227ecf92cd9b57
Raqin Shikder <raqin2003@gmail.com>
2022-05-18 11:56:21 -0700

    created first iteration of category search function

--- modules/catsearch.py
+++ modules/catsearch.py
@@ -6,4 +6,10 @@ def category_search(rlist: list[dict], cat: list[str]) -> list[dict]:
     """
     Searches through the recipe list(rlist) and returns recipes(dict) that match the category(s)(cat)
     """
-    pass
+    cor_recipes = []
+    for recipe in rlist:
+        for rlist['diet'] in recipe:
+            if cat in rlist['diet']:
+                cor_recipes.append[recipe]
+
+   
