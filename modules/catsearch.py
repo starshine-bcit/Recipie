2a06bceee1b86eb54b22eabc9311d6b8e1a6e385
Amanda Chang <achang140@my.bcit.ca>
2022-05-18 12:32:08 -0700

    Added category_search function to catsearch.py

--- modules/catsearch.py
+++ modules/catsearch.py
@@ -6,10 +6,12 @@ def category_search(rlist: list[dict], cat: list[str]) -> list[dict]:
     """
     Searches through the recipe list(rlist) and returns recipes(dict) that match the category(s)(cat)
     """
-    cor_recipes = []
-    for recipe in rlist:
-        for rlist['diet'] in recipe:
-            if cat in rlist['diet']:
-                cor_recipes.append[recipe]
+    matched_recipe = []
+    for recipe in rlist: 
+        for item in recipe["diets"]:
+            if item in cat: 
+                matched_recipe.append(recipe)
+    
+    return matched_recipe
 
    
