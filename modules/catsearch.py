ede90b16bc73a16c2db428f76b20f6704ebfdde2
Sean <Waffuruki@hotmail.com>
2022-05-24 11:53:53 -0700

    Updated catsearch module's docstring, fixed the test jsons

--- modules/catsearch.py
+++ modules/catsearch.py
@@ -1,14 +1,20 @@
 """A function that takes a list of categories and a list of recipes,
 returns a list of recipes that have the same category matches."""
 
-import string
 from .recipe import Recipe
 from .recipelist import RecipeList
 
 
 def category_search(cat: list[str], rlist: RecipeList) -> list[Recipe]:
     """
-    Searches through the recipe list(rlist) and returns recipes(dict) that match the category(s)(cat)
+    Takes a RecipeList (rlist) instance and compares the recipe instances categories if they match the categories (cat)
+
+    Args:
+        cat (list[str]): The list of categories that the users want filtered back to them
+        rlist (RecipeList): The RecipeList instance, holds the recipes inside it
+
+    Returns:
+        list[Recipe]: A list of recipe instances
     """
     matched_recipe = []
     for recipe in rlist.recipes: 
